9b29e850d1977bedf9bdfb2354f57c4afcc1200d
RomanKuranov <romankuranov93@gmail.com>
2023-02-20 23:00:18 +0200

    task -1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// const ulEl = document.querySelector('.gallery');
+
+// const creatgalleryItems = (galleryItems) => {
+//     return galleryItems
+//     .map(({preview, original, description}) => {
+//         return `
+//         <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>`;
+//     }).join('');
+// };
+// const galleryMarkup = ulEl.insertAdjacentHTML('beforeend', creatgalleryItems(galleryItems));
+
+// let gallery = new SimpleLightbox('.gallery a', { 
+// captionsData: 'alt', 
+// captionPosition:'bottom',
+// captionDelay: 250 });
 
 console.log(galleryItems);
