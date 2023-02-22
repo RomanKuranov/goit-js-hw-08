ab06b51be20935a32997eb38b272d1094569d2f9
RomanKuranov <romankuranov93@gmail.com>
2023-02-22 13:51:25 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,27 @@
+
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// const ulEl = document.querySelector('.gallery');
+const ulEl = document.querySelector('.gallery');
 
-// const creatgalleryItems = (galleryItems) => {
-//     return galleryItems
-//     .map(({preview, original, description}) => {
-//         return `
-//         <a class="gallery__item" href="${original}">
-//         <img class="gallery__image" src="${preview}" alt="${description}" />
-//       </a>`;
-//     }).join('');
-// };
-// const galleryMarkup = ulEl.insertAdjacentHTML('beforeend', creatgalleryItems(galleryItems));
+const creatgalleryItems = (galleryItems) => {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+};
+const galleryMarkup = ulEl.insertAdjacentHTML('beforeend', creatgalleryItems(galleryItems));
 
-// let gallery = new SimpleLightbox('.gallery a', { 
-// captionsData: 'alt', 
-// captionPosition:'bottom',
-// captionDelay: 250 });
+let gallery = new SimpleLightbox('.gallery a', { 
+captionsData: 'alt', 
+captionPosition:'bottom',
+captionDelay: 250 });
 
 console.log(galleryItems);
