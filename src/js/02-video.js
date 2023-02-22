c92a4e7b49ba8c4ad259e61c7a715db09c0f41a2
RomanKuranov <romankuranov93@gmail.com>
2023-02-22 18:46:49 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+reloadPage();
+
+player.on('timeupdate', function(time) {
+    localStorage.setItem(CURRENT_TIME, time.seconds);
+ });
+
+function reloadPage() {
+    const savedLocalTime = localStorage.getItem(CURRENT_TIME);
+
+    if (savedLocalTime) {
+        player.setCurrentTime(savedLocalTime);
+    }
+}
